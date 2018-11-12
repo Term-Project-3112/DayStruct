a8fe839a7c75534a6fd163654ff2a92fb00df8e3
Allie Beckman <alliemcanally@gmail.com>
2018-11-12 15:06:44 -0500

    New Directories and a Doc folder
    Directories that set up the project output and a docs folder to explain some of the layout.

---
+++ Driver/include/html_output.h
@@ -0,0 +1,17 @@
+#ifndef HTML_OUTPUT_H
+#define HTML_OUTPUT_H
+
+
+class html_output
+{
+    public:
+        html_output();
+        createTestPage();
+        virtual ~html_output();
+
+    protected:
+
+    private:
+};
+
+#endif // HTML_OUTPUT_H
--- Driver/main.cpp
+++ Driver/main.cpp
@@ -1,55 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include "conDB.h"
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
+#include "html_output.h"
 
-const string ENV[ 24 ] = {
-   "COMSPEC", "DOCUMENT_ROOT", "GATEWAY_INTERFACE",
-   "HTTP_ACCEPT", "HTTP_ACCEPT_ENCODING",
-   "HTTP_ACCEPT_LANGUAGE", "HTTP_CONNECTION",
-   "HTTP_HOST", "HTTP_USER_AGENT", "PATH",
-   "QUERY_STRING", "REMOTE_ADDR", "REMOTE_PORT",
-   "REQUEST_METHOD", "REQUEST_URI", "SCRIPT_FILENAME",
-   "SCRIPT_NAME", "SERVER_ADDR", "SERVER_ADMIN",
-   "SERVER_NAME","SERVER_PORT","SERVER_PROTOCOL",
-   "SERVER_SIGNATURE","SERVER_SOFTWARE" };
+using namespace std;
 
 int main () {
 
-    ofstream testPageOne;
-    testPageOne.open ("G:/My Drive/ITCS 3112 Term Project/UI_Docs(HTML, CSS, Javascript files)/HTML/testPageOne.html");
+    html_output testOP;
 
-    //add some html content
-   testPageOne << "Content-type:text/html\r\n\r\n";
-   testPageOne << "<html>\n";
-   testPageOne << "<head>\n";
-   testPageOne << "<title>CGI Environment Variables</title>\n";
-   testPageOne << "</head>\n";
-   testPageOne << "<body>\n";
-   testPageOne << "<table border = \"0\" cellspacing = \"2\">";
-
-   for ( int i = 0; i < 24; i++ ) {
-      testPageOne << "<tr><td>" << ENV[ i ] << "</td><td>";
-
-      // attempt to retrieve value of environment variable
-      char *value = getenv( ENV[ i ].c_str() );
-      if ( value != 0 ) {
-         testPageOne << value;
-      } else {
-         testPageOne << "Environment variable does not exist.";
-      }
-      testPageOne << "</td></tr>\n";
-   }
-
-   testPageOne << "</table><\n";
-   testPageOne << "</body>\n";
-   testPageOne << "</html>\n";
-
-   //ending html
-    testPageOne << "</body></html>";
-    testPageOne.close();
+    testOP.createTestPage();
 
    return 0;
 }
